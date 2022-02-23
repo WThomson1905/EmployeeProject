5f254f6567362377252ea8c6aeb5b25b575addbf
William Thomson <99246579+WThomson1905@users.noreply.github.com>
2022-02-23 14:13:34 +0000

    Added a few unit tests and restruftured ValidChangeEmployeePositionRequest method.

--- EmployeeProjectUnitTests/EmployeeProjectTests.cs
+++ EmployeeProjectUnitTests/EmployeeProjectTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EmployeeProject;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace EmployeeProjectUnitTests
 {
@@ -81,21 +83,20 @@ namespace EmployeeProjectUnitTests
         };
         EmployeeController controller = new EmployeeController();
 
-
         [TestMethod]
         public void AddEmployee_AddingNewValidEmployee_AddNewValue()
-        {            
+        {
             // Arrange
             Employee employee = new Employee()
             {
                 EmployeeId = 100,
                 Forename = "John",
-                Surname = "Snow", 
+                Surname = "Snow",
                 Email = "js@gmail.com",
                 Position = 0
             };
 
-            // Arrange 
+            // Act 
             bool result = controller.AddEmployee(employee, allEmployees).Result;
 
             // Assert
@@ -103,7 +104,7 @@ namespace EmployeeProjectUnitTests
         }
 
         [TestMethod]
-        public void AddEmployee_AddingNewInvalidEmployee_ShouldNotAdd()
+        public void AddEmployee_EmailNotValid_ShouldNotAdd()
         {
             // Arrange - invalid email
             Employee employee = new Employee()
@@ -115,8 +116,28 @@ namespace EmployeeProjectUnitTests
                 Position = 0
             };
 
+            // Act 
+            bool result = controller.AddEmployee(employee, allEmployees).Result;
 
-            // Arrange 
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void AddEmployee_PositionNotValid_ShouldNotAdd()
+        {
+            // Arrange - invalid email
+            Employee employee = new Employee()
+            {
+                EmployeeId = 101,
+                Forename = "Bill",
+                Surname = "Nye",
+                Email = "bn@gmail.com",
+                Position = (EmployeeType)5
+            };
+
+
+            // Act 
             bool result = controller.AddEmployee(employee, allEmployees).Result;
 
             // Assert
@@ -130,74 +151,225 @@ namespace EmployeeProjectUnitTests
         {
             // Arrange
             List<Employee> toUpdateEmployees = new List<Employee>
+            {
+                new Employee()
                 {
-                    new Employee()
-                    {
-                        EmployeeId = 1,
-                        Forename = "Tom",
-                        Surname = "Cruise",
-                        Email = "tc@g.com",
-                        Position = EmployeeType.Manager
-                    },
-                    new Employee()
-                    {
-                        EmployeeId = 2,
-                        Forename = "Brad",
-                        Surname = "Pitt",
-                        Email = "bp@g.com",
-                        Position = EmployeeType.Engineer
-                    },
-                    new Employee()
-                    {
-                        EmployeeId = 3,
-                        Forename = "Bill",
-                        Surname = "Sandler",
-                        Email = "bs@g.com",
-                        Position = EmployeeType.Intern
-                    },
-                    new Employee()
-                    {
-                        EmployeeId = 4,
-                        Forename = "Jack",
-                        Surname = "Sheppard",
-                        Email = "js@g.com",
-                        Position = EmployeeType.Engineer
-                    },
-                    new Employee()
-                    {
-                        EmployeeId = 5,
-                        Forename = "John",
-                        Surname = "Locke",
-                        Email = "jl@g.com",
-                        Position = EmployeeType.Engineer
-                    },
-                    new Employee()
-                    {
-                        EmployeeId = 6,
-                        Forename = "Kate",
-                        Surname = "Auston",
-                        Email = "ka@g.com",
-                        Position = EmployeeType.Engineer
-                    },
-                    new Employee()
-                    {
-                        EmployeeId = 7,
-                        Forename = "Ahhhhhhhh",
-                        Surname = "Smith",
-                        Email = "js@g.com",
-                        Position = EmployeeType.Intern
-                    }
-                };
+                    EmployeeId = 1,
+                    Forename = "Tom",
+                    Surname = "Cruise",
+                    Email = "tc@g.com",
+                    Position = EmployeeType.Manager
+                },
+                new Employee()
+                {
+                    EmployeeId = 2,
+                    Forename = "Brad",
+                    Surname = "Pitt",
+                    Email = "bp@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 3,
+                    Forename = "Bill",
+                    Surname = "Sandler",
+                    Email = "bs@g.com",
+                    Position = EmployeeType.Intern
+                },
+                new Employee()
+                {
+                    EmployeeId = 4,
+                    Forename = "Jack",
+                    Surname = "Sheppard",
+                    Email = "js@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 5,
+                    Forename = "John",
+                    Surname = "Locke",
+                    Email = "jl@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 6,
+                    Forename = "Kate",
+                    Surname = "Auston",
+                    Email = "ka@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 7,
+                    Forename = "Ahhhhhhhh",
+                    Surname = "Smith",
+                    Email = "js@g.com",
+                    Position = EmployeeType.Intern
+                }
+            };
             var employeeId = 1;
 
-            // Arrange 
-            List<Employee> employees = controller.DeleteEmployeeMK2(employeeId, toUpdateEmployees).Result;
+            // Act 
+            List<Employee> employees = controller.DeleteEmployee(employeeId, toUpdateEmployees).Result;
 
             // Assert
             CollectionAssert.AreNotEqual(allEmployees, employees);
         }
 
 
+        [TestMethod]
+        public void DeleteEmployee_DeletingNewEmployee_ShouldNotMatchMK2()
+        {
+            // Arrange
+            List<Employee> toUpdateEmployees = new List<Employee>
+            {
+                new Employee()
+                {
+                    EmployeeId = 1,
+                    Forename = "Tom",
+                    Surname = "Cruise",
+                    Email = "tc@g.com",
+                    Position = EmployeeType.Manager
+                },
+                new Employee()
+                {
+                    EmployeeId = 2,
+                    Forename = "Brad",
+                    Surname = "Pitt",
+                    Email = "bp@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 3,
+                    Forename = "Bill",
+                    Surname = "Sandler",
+                    Email = "bs@g.com",
+                    Position = EmployeeType.Intern
+                },
+                new Employee()
+                {
+                    EmployeeId = 4,
+                    Forename = "Jack",
+                    Surname = "Sheppard",
+                    Email = "js@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 5,
+                    Forename = "John",
+                    Surname = "Locke",
+                    Email = "jl@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 6,
+                    Forename = "Kate",
+                    Surname = "Auston",
+                    Email = "ka@g.com",
+                    Position = EmployeeType.Engineer
+                },
+                new Employee()
+                {
+                    EmployeeId = 7,
+                    Forename = "Ahhhhhhhh",
+                    Surname = "Smith",
+                    Email = "js@g.com",
+                    Position = EmployeeType.Intern
+                }
+            };
+            var employeeId = 1;
 
+            // Act 
+            List<Employee> employees = controller.DeleteEmployee(employeeId, toUpdateEmployees).Result;
+            bool areEqual = allEmployees.Equals(employees);
+
+            // Assert
+            Assert.IsFalse(areEqual);
+        }
+
+
+
+
+        [TestMethod]
+        public void DeleteEmployee_DeletingNonExistentEmployee_ShouldNotFind()
+        {
+            // Arrange
+            string path = "C:\\Users\\William\\source\\repos\\EmployeeProject\\EmployeeProject\\Employees.json";
+            List<Employee> allEmployees = controller.DeserizalizeEmployeeJson(path);
+            var employeeId = 100000;
+            bool couldFindId = true;
+
+            // Act 
+            if (allEmployees.Any(e => e.EmployeeId == employeeId))
+            {
+                List<Employee> deletedEmployee = controller.DeleteEmployee(employeeId, allEmployees).Result;
+                couldFindId = true;
+            }
+            else
+            {
+                couldFindId = false;
+            }
+
+            // Assert
+            Assert.IsFalse(couldFindId);
+        }
+
+
+        [TestMethod]
+        public void DeserizalizeEmployeeJson_DeserizalizeEmployeeJson_ShouldNotFind()
+        {
+            // Arrange
+            string path = @"C:\Users\William\source\repos\EmployeeProject\EmployeeProject\TestJsonFiles\EmployeesInvalid.json";
+
+
+            // Act 
+            List<Employee> deserialized = controller.DeserizalizeEmployeeJson(path);
+
+      
+            // Assert
+            Assert.IsNotNull(deserialized);
+        }
+
+        //[TestMethod]
+        //public void UpdateEmployeeAsync_ChangeEngineerToIntern_ReturnFalse()
+        //{
+        //    // Arrange
+        //    EmployeeType employeeType = EmployeeType.Intern;
+        //    var employeeId = 2; // Engineer
+        //    bool changedSuccessfully;
+
+        //    // Act 
+        //    if (controller.UpdateEmployeeAsync(employeeId, employeeType).Result)
+        //    {
+        //        changedSuccessfully = true;
+        //    }
+        //    else
+        //    {
+        //        changedSuccessfully = false;
+        //    }
+
+        //    // Assert
+        //    Assert.IsFalse(changedSuccessfully);
+        //}
+
+        //[TestMethod]
+        //public void UpdateEmployeeAsync_ChangeManagerToIntern_ReturnFalse()
+        //{
+        //    // Arrange
+        //    EmployeeType employeeType = EmployeeType.Intern;
+        //    var employeeId = 1; // Manager
+        //    Display display = new Display();
+
+        //    // Act
+        //    bool valid = display.ValidChangeEmployeePositionRequest(employeeId, employeeType);
+
+        //    // Assert
+        //    Assert.IsFalse(valid);
+        //}
     }
 }
