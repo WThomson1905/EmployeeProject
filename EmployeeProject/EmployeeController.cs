99edda2ed57a9e154bec8b83ad373e542f697b61
William Thomson <99246579+WThomson1905@users.noreply.github.com>
2022-02-25 13:38:08 +0000

    bug fix - update employee not showing cuyrrent json data

--- EmployeeProject/EmployeeController.cs
+++ EmployeeProject/EmployeeController.cs
@@ -90,10 +90,6 @@ namespace EmployeeProject
                 };
                 await SerializeToFile(employees, path);
             }
-            else
-            {
-                employees = DeserizalizeEmployeeJson(path);
-            }
 
             return employees;
         }
@@ -103,7 +99,7 @@ namespace EmployeeProject
 
         public List<Employee> FilterEmployees(EmployeeType position, List<Employee> employees)
         {
-            //List<Employee> employees = DeserizalizeEmployeeJson(path);
+            List<Employee> employees = DeserizalizeEmployeeJson(path);
             List<Employee> employeesFileteredByPosition = new List<Employee>();
 
             foreach (Employee employee in employees)
